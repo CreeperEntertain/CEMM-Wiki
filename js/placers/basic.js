98b148bd5bed9d323a5cd5b6ed24e28e761fcb01
CEntertain <TimGeru@gmx.de>
2025-09-13 23:12:16 +0200

    Update basic.js

--- js/placers/basic.js
+++ js/placers/basic.js
@@ -9,33 +9,9 @@
     // Insert the HTML before the current script tag
     scriptTag.insertAdjacentHTML('beforebegin', html);
 
+    // Re-execute any <script> tags inside the inserted HTML
     const host = window.location.hostname;
     const isLocal = (host === "localhost" || host === "127.0.0.1");
-
-    // Adjust paths in inserted content
-    if (!isLocal) {
-        const container = scriptTag.parentElement;
-
-        // Elements and their attributes to check
-        const attrMap = {
-            "img": "src",
-            "link": "href",
-            "video": "src",
-            "audio": "src",
-            "source": "src"
-        };
-
-        for (const [tag, attr] of Object.entries(attrMap)) {
-            for (const el of container.querySelectorAll(`${tag}[${attr}]`)) {
-                let val = el.getAttribute(attr);
-                if (val && !val.startsWith("http") && !val.startsWith("/") && !val.startsWith("CEMM-Wiki/")) {
-                    el.setAttribute(attr, "CEMM-Wiki/" + val);
-                }
-            }
-        }
-    }
-
-    // Re-execute any <script> tags inside the inserted HTML
     for (const oldScript of [...scriptTag.parentElement.querySelectorAll("script[src]")]) {
         if (oldScript === scriptTag) continue;
         const newScript = document.createElement("script");
