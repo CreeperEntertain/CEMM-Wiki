f7965d5884ef8e33d6c65a4d247ef3c1f3e23a10
CEntertain <TimGeru@gmx.de>
2025-09-18 18:42:14 +0200

    Tests

--- js/elements/title-element.js
+++ js/elements/title-element.js
@@ -5,9 +5,8 @@ export class TitleElement extends HTMLElement {
     }
 
     async render() {
-        const response = await fetch('/js/elements/containers/title-element.html');
+        const response = await fetch('/CEMM-Wiki/js/elements/containers/title-element');
         const html = await response.text();
-        // Inject directly into the custom element itself
         this.innerHTML = html;
     }
 } customElements.define("title-element", TitleElement);
