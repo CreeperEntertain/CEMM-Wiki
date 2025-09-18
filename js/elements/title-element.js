dded45692bf41e6d649a533d6a0316a15e3e6a15
CEntertain <TimGeru@gmx.de>
2025-09-18 19:02:23 +0200

    Typo

--- js/elements/title-element.js
+++ js/elements/title-element.js
@@ -8,7 +8,7 @@ export class TitleElement extends HTMLElement {
     async render(fileToAwait) {
         const host = window.location.hostname;
         const isLocal = (host === "localhost" || host === "127.0.0.1");
-        const response = await fetch(isLocal ? fileToAwait : 'CEMM-Wiki/' + fileToAwait);
+        const response = await fetch(isLocal ? fileToAwait : '/CEMM-Wiki' + fileToAwait);
         const html = await response.text();
         this.innerHTML = html;
     }
