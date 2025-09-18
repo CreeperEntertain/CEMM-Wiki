44778ac56668f5dea597b456b38ea517c547c4cc
CEntertain <TimGeru@gmx.de>
2025-09-18 18:48:33 +0200

    Update title-element.js

--- js/elements/title-element.js
+++ js/elements/title-element.js
@@ -1,11 +1,13 @@
 export class TitleElement extends HTMLElement {
     constructor() {
         super();
-        this.render();
+        this.render('/js/elements/containers/title-element');
     }
 
-    async render() {
-        const response = await fetch('/CEMM-Wiki/js/elements/containers/title-element');
+    async render(fileToAwait) {
+        const host = window.location.hostname;
+        const isLocal = (host === "localhost" || host === "127.0.0.1");
+        const response = await fetch(isLocal ? fileToAwait : 'CEMM-Wiki/' + fileToAwait);
         const html = await response.text();
         this.innerHTML = html;
     }
