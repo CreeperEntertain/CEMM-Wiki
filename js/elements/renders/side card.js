3711ed328fec9f7f2fa568a36f0f4e301010a089
CEntertain <TimGeru@gmx.de>
2025-09-19 14:22:48 +0200

    Updated side card

---
+++ js/elements/renders/side card.js
@@ -0,0 +1,24 @@
+export class SideCardRender {
+    static async render(script, fileToAwait, jsonToAwait) {
+        const host = window.location.hostname;
+        const isLocal = (host === "localhost" || host === "127.0.0.1");
+
+        const response = await fetch(isLocal ? fileToAwait : '/CEMM-Wiki' + fileToAwait);
+        let html = await response.text();
+
+        const replacements = {
+            TEXT: script.getAttribute('text') || '',
+            ICON: script.getAttribute('icon') || '',
+            LINK: script.getAttribute('link') || '',
+        };
+
+        for (const [key, value] of Object.entries(replacements)) {
+            const regex = new RegExp(`\\$\\{${key}\\}`, 'g');
+            html = html.replace(regex, value);
+        }
+
+        html = html.replace(/\$\{ROOT\}/g, isLocal ? '/' : '/CEMM-Wiki/');
+        
+        script.innerHTML = html;
+    }
+}
--- js/elements/side-card.js
+++ js/elements/side-card.js
@@ -1,9 +1,11 @@
-import { BasicRenderer } from "./renders/basic.js";
+import { SideCardRender } from "./renders/side card.js";
 export class SideCard extends HTMLElement {
     fileToAwait = '/html/templates/placed/side-card.html';
+    jsonToAwait = '/json/side card elements.json';
+
     constructor() {
         super();
-        BasicRenderer.render(this, this.fileToAwait);
+        SideCardRender.render(this, this.fileToAwait, this.jsonToAwait);
     }
 }
 customElements.define("side-card", SideCard);
